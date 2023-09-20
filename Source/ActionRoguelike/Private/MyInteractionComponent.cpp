82adc92a3000cdc22fe8e68c5806b1ba06a7332b
JRjie <1261536301@qq.com>
2023-09-21 00:46:38 +0800

    Added actual interactcomp

---
+++ Source/ActionRoguelike/Private/MyInteractionComponent.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyInteractionComponent.h"
+#include "MyGameplayInterface.h"
+
+// Sets default values for this component's properties
+UMyInteractionComponent::UMyInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMyInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMyInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UMyInteractionComponent::PrimaryInteract()
+{
+	FCollisionObjectQueryParams ObjectQueryParams;
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+
+	AActor* Owner = GetOwner();//** who use this interaction component
+
+	FVector Start;
+	FVector End;
+	FVector EyeLocation;
+	FRotator EyeRotation;
+	Owner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+	Start = EyeLocation;
+	End = EyeLocation + (EyeRotation.Vector() * 1000.0f);
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(Hit, Start, End, ObjectQueryParams);
+
+	AActor* HitActor = Hit.GetActor();
+	if (HitActor != nullptr)
+	{
+		if (HitActor->Implements<UMyGameplayInterface>())
+		{
+			APawn* Pawn = Cast<APawn>(Owner);
+
+			IMyGameplayInterface::Execute_Interact(HitActor, Pawn);
+
+		}
+	}
+}
+
