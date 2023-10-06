cd83d412db04d34ef4d594c227aab533ce2b1cc0
JRjie <1261536301@qq.com>
2023-10-06 21:50:40 +0800

    added base class for projectile

---
+++ Source/ActionRoguelike/Private/MyProjectileBase.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyProjectileBase.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AMyProjectileBase::AMyProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp->SetCollisionProfileName("Projectile");
+	RootComponent = SphereComp;
+	SphereComp->OnComponentHit.AddDynamic(this, &AMyProjectileBase::OnActorHit);
+
+	EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
+	EffectComp->SetupAttachment(RootComponent);
+
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
+	MovementComp->InitialSpeed = 8000.0f;
+	MovementComp->bRotationFollowsVelocity = true;
+	MovementComp->bInitialVelocityInLocalSpace = true;
+	MovementComp->ProjectileGravityScale = 0.0f;
+}
+
+void AMyProjectileBase::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Explode();
+}
+
+void AMyProjectileBase::Explode_Implementation()
+{
+	if (ensure(!IsPendingKill()))
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(this, ImpactVFX, GetActorLocation(), GetActorRotation());
+		
+		Destroy();
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+//void AMyProjectileBase::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
---
+++ Source/ActionRoguelike/Public/MyProjectileBase.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyProjectileBase.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;	
+class UParticleSystemComponent;
+
+UCLASS(ABSTRACT)
+class ACTIONROGUELIKE_API AMyProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyProjectileBase();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* ImpactVFX;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	USphereComponent* SphereComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UProjectileMovementComponent* MovementComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	UParticleSystemComponent* EffectComp;
+
+	UFUNCTION()
+	virtual void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Explode();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+//public:	
+	// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+
+};
